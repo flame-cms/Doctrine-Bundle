fbe5189e7fe00119f37e5e7f07181840bf4a2c75
JSifalda <sifalda.jiri@gmail.com>
2013-07-13 16:36:47 +0200

    Add source

---
+++ DoctrineBundle/DoctrineBundle.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Class DoctrineBundle
+ *
+ * @author: Jiří Šifalda <sifalda.jiri@gmail.com>
+ * @date: 13.07.13
+ */
+namespace Flame\CMS\DoctrineBundle;
+
+use Flame\Bundles\Bundle;
+use Kdyby\Console\DI\ConsoleExtension;
+use Kdyby\Doctrine\DI\OrmExtension;
+use Kdyby\Events\DI\EventsExtension;
+
+class DoctrineBundle extends Bundle
+{
+
+	/**
+	 * @return array
+	 */
+	public function getConfigFiles()
+	{
+		return array(
+			$this->getCurrentPath() . '/config/config.neon',
+			$this->getCurrentPath() . '/config/config.local.neon'
+		);
+	}
+
+	protected function beforeSetUp()
+	{
+		ConsoleExtension::register($this->getConfigurator());
+		OrmExtension::register($this->getConfigurator());
+		EventsExtension::register($this->getConfigurator());
+	}
+}
